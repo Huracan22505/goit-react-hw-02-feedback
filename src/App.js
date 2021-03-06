b2c288bb09a8d0d41406996b5aa7ee2a4761a7b1
Huracan <huracan22505@protonmail.com>
2021-03-06 08:51:08 +0200

    feat: create App component, add logic for other components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,54 @@
-export default function App() {
-  return <></>;
+import React, { Component } from 'react';
+
+import Statistics from 'components/Statistics';
+import FeedbackOptions from 'components/FeedbackOptions';
+import Section from 'components/Section';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  feedbackIncrement = e => {
+    e = e.target.textContent.toLowerCase();
+    this.setState(prevState => ({
+      [e]: prevState[e] + 1,
+    }));
+  };
+
+  countTotalFeedback = (good, neutral, bad) => good + neutral + bad;
+
+  countPositiveFeedbackPercentage = (good, neutral, bad) =>
+    Math.round((good * 100) / this.countTotalFeedback(good, neutral, bad));
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = this.countTotalFeedback(good, neutral, bad);
+
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            // options={}
+            onLeaveFeedback={this.feedbackIncrement}
+          />
+        </Section>
+        <Section title="Statistics">
+          {totalFeedback > 0 && (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedback}
+              positivePercentage={this.countPositiveFeedbackPercentage}
+            />
+          )}
+        </Section>
+      </>
+    );
+  }
 }
+
+export default App;
