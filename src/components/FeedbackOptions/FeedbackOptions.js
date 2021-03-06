d011525c3687da52b90feb9aaa4c43c7b102b201
Huracan <huracan22505@protonmail.com>
2021-03-06 08:50:21 +0200

    feat: add FO component and add re exp method

---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const FeedbackOptions = ({ onLeaveFeedback }) => {
+  return (
+    <>
+      <ul>
+        <li>
+          <button type="button" onClick={onLeaveFeedback}>
+            Good
+          </button>
+        </li>
+        <li>
+          <button type="button" onClick={onLeaveFeedback}>
+            Neutral
+          </button>
+        </li>
+        <li>
+          <button type="button" onClick={onLeaveFeedback}>
+            Bad
+          </button>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default FeedbackOptions;
